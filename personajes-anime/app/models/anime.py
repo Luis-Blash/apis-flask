aaeb32c41719d98d4afd131b204ea8a48b6855fa
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2021-05-11 17:10:45 -0500

    Creada la tabla de animes

--- personajes-anime/app/models/anime.py
+++ personajes-anime/app/models/anime.py
@@ -4,8 +4,19 @@ class Anime(db.Model):
     __tablename__ = 'anime'
     
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(30))
+    anime = db.Column(db.String(30))
+    temporada = db.Column(db.Integer)
+    fecha_publicacion = db.Column(db.String(15))
+    fecha_termino = db.Column(db.String(15))
+    capitulos = db.Column(db.Integer)
+    estado = db.Column(db.Boolean)
 
-    def __init__(self, nombre):
+
+    def __init__(self, nombre,temporada, fecha_publicacion, fecha_termino, capitulos, estado):
         self.nombre = nombre
+        self.temporada = temporada
+        self.fecha_publicacion = fecha_publicacion
+        self.fecha_termino = fecha_termino
+        self.capitulos = capitulos
+        self.estado = estado
 
