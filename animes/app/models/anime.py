e5cace1d5f9191db02990dc7f9f7fc290e92ee4d
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2021-05-12 21:41:07 -0500

    Optimizacion de codigo

--- animes/app/models/anime.py
+++ animes/app/models/anime.py
@@ -1,5 +1,5 @@
 from app import db
-
+import json
 
 class Anime(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,3 +9,78 @@ class Anime(db.Model):
     fecha_termino = db.Column(db.String(15))
     capitulos = db.Column(db.Integer)
     estado = db.Column(db.Boolean)
+
+    def __init__(self, anime=None, temporada=None, fecha_publicacion=None, fecha_termino=None, capitulos=None, estado=None):
+        self.anime =  anime
+        self.temporada =  temporada
+        self.fecha_termino =  fecha_termino
+        self.fecha_publicacion =  fecha_publicacion
+        self.capitulos =  capitulos
+        self.estado =  estado
+        self.datos = []
+        self.animes = ''
+
+    def anime_post(self):
+        if type(self.estado) is bool and type(self.temporada) is int and type(self.capitulos) is int:
+            agregar = Anime(
+                anime= self.anime,
+                temporada= self.temporada,
+                fecha_publicacion= self.fecha_publicacion,
+                fecha_termino= self.fecha_termino,
+                capitulos= self.capitulos,
+                estado= self.estado
+                )
+            db.session.add(agregar)
+            db.session.commit()
+            return {'mensaje':f"Se inserto {self.anime}"}
+        else:
+            return {'mensaje':"Falta datos"}
+
+    def anime_query_all(self):
+        self.animes = Anime.query.all()
+        self.__json_datos_list()
+        return json.dumps(self.datos)
+
+    def anime_search(self,nombre):
+        self.animes = Anime.query.filter(Anime.anime.like(f'%{nombre}%')).all()
+        self.__json_datos_list()
+        return json.dumps(self.datos)
+
+    def __json_datos_list(self):
+        for i in self.animes:
+            self.datos.append(
+                {
+                    "id": i.id,
+                    "anime": i.anime,
+                    "temporada": i.temporada,
+                    "fecha_publicacion": i.fecha_publicacion,
+                    "fecha_termino": i.fecha_publicacion,
+                    "capitulos": i.capitulos,
+                    "estado": i.estado
+                }
+            )
+
+
+    def anime_one(self,anime):
+        if type(anime) is str:
+            try:
+                self.animes = Anime.query.filter_by(anime=anime).first()
+                return self.__json_datos_one()
+            except AttributeError:
+                return False
+        else:
+            self.animes = Anime.query.filter_by(id=anime).first()
+            return self.animes
+
+
+    def __json_datos_one(self):
+        datos = {
+            "id": self.animes.id,
+            "anime": self.animes.anime,
+            "temporada": self.animes.temporada,
+            "fecha_publicacion": self.animes.fecha_publicacion,
+            "fecha_termino": self.animes.fecha_publicacion,
+            "capitulos": self.animes.capitulos,
+            "estado": self.animes.estado
+        } 
+        return datos
--- animes/app/views/home.py
+++ animes/app/views/home.py
@@ -1,3 +1,4 @@
+import re
 from flask import Blueprint, jsonify, request
 from flask.helpers import make_response
 from app.models.anime import Anime
@@ -18,73 +19,47 @@ def anime_all():
     nombre = request.args.get('nombre', default=None, type=None)
     datos = []
     if nombre is None:
-        animes = Anime.query.all()
-        for i in animes:
-            json_datos = {
-                "id": i.id,
-                "anime": i.anime,
-                "temporada": i.temporada,
-                "fecha_publicacion": i.fecha_publicacion,
-                "fecha_termino": i.fecha_publicacion,
-                "capitulos": i.capitulos,
-                "estado": i.estado
-            }
-            datos.append(json_datos)
-        datos = json.dumps(datos)
+        datos = Anime().anime_query_all()
         respuesta = make_response(datos, 200)
         respuesta.headers['Content-Type'] = 'application/json'
         return respuesta
     else:
-        busqueda = Anime.query.filter(Anime.anime.like(f'%{nombre}%')).all()
-        for i in busqueda:
-            json_datos = {
-                "id": i.id,
-                "anime": i.anime,
-                "temporada": i.temporada,
-                "fecha_publicacion": i.fecha_publicacion,
-                "fecha_termino": i.fecha_publicacion,
-                "capitulos": i.capitulos,
-                "estado": i.estado
-            }
-            datos.append(json_datos)
-        datos = json.dumps(datos)
+        nombre = nombre.lower()
+        datos = Anime().anime_search(nombre)
         respuesta = make_response(datos, 200)
         respuesta.headers['Content-Type'] = 'application/json'
         return respuesta
 
 
 @home.route('/anime/<anime>', methods=['GET'])
-def anime_id(anime: str):
+def anime_id(anime):
     '''Metodo GET uno'''
+    anime = anime.lower()
     try:
-        anime = anime.lower()
-        query_anime = Anime.query.filter_by(anime=anime).first()
-        json_datos = {
-                "id": query_anime.id,
-                "anime": query_anime.anime,
-                "temporada": query_anime.temporada,
-                "fecha_publicacion": query_anime.fecha_publicacion,
-                "fecha_termino": query_anime.fecha_publicacion,
-                "capitulos": query_anime.capitulos,
-                "estado": query_anime.estado
-        }
-        respuesta = make_response(json_datos, 200)
+        datos = Anime().anime_one(anime)
+        respuesta = make_response(datos, 200)
         respuesta.headers['Content-Type'] = 'application/json'
         return respuesta
-    except AttributeError:
-        return jsonify({'mensaje': f'No es String: {anime}'}), 400
+    except (AttributeError, TypeError):
+        return jsonify({'mensaje': 'No existe'}), 404
 
 
 @home.route('/anime/<id>', methods=['DELETE'])
 def anime_delete(id: int):
     '''Metodo DELETE'''
-    anime_delete = Anime.query.filter_by(id=id).first()
-    if anime_delete:
-        db.session.delete(anime_delete)
-        db.session.commit()
-        return jsonify({'mensaje': 'Eliminado'}), 200
-    else:
-        return jsonify({'mensaje': 'No existe'}), 404
+    try:
+        id = int(id)
+        anime_delete = Anime().anime_one(id)
+        print(anime_delete)
+        if anime_delete:
+            db.session.delete(anime_delete)
+            db.session.commit()
+            return jsonify({'mensaje': 'Eliminado'}), 200
+        else:
+            return jsonify({'mensaje': 'No existe id'}), 404
+    except (AttributeError, ValueError):
+        return jsonify({'mensaje': f'Error'}), 404
+
 
 
 @home.route('/anime', methods=['POST'])
@@ -92,27 +67,13 @@ def anime_post():
     '''Metodo Post '''
     data = request.get_json()
     nombre = data['anime'].lower()
-    anime = Anime.query.filter_by(anime=nombre).first()
+    anime = Anime().anime_one(nombre)
     if anime or nombre == '':
         return jsonify({'mensaje': f'Existe: {nombre}'})
     else:
-        try:
-            temporada = data['temporada']
-            fecha_publicacion = data['fecha_publicacion']
-            fecha_termino = data['fecha_termino']
-            capitulos = data['capitulos']
-            estado = data['estado']
-            if type(temporada) == int and type(fecha_termino) == str and type(fecha_publicacion) == str and type(estado) == bool and type(estado) == bool:
-                agregar = Anime(
-                    anime=nombre,
-                    temporada=temporada,
-                    fecha_publicacion=fecha_publicacion,
-                    fecha_termino=fecha_termino,
-                    capitulos=capitulos,
-                    estado=estado
-                    )
-                db.session.add(agregar)
-                db.session.commit()
-            return jsonify({'mensaje': f'Se inserto: {nombre}'}), 201
-        except TypeError:
-            return jsonify({'mensaje': 'Verifica los nombres de las key'}), 400
+        insertar = Anime(nombre, data['temporada'],
+        data['fecha_publicacion'], data['fecha_termino'], 
+        data['capitulos'], data['estado'])
+        respuesta = make_response(insertar.anime_post(), 200)
+        respuesta.headers['Content-Type'] = 'application/json'
+        return respuesta
